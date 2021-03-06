9fc268cfedf3e3688c886a1663eed620cee9b533
Victor Ihedioha <ihedioha.victor@gmail.com>
2021-03-06 04:07:57 +0100

    initial commit

---
+++ functional_test.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import unittest
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+cap = DesiredCapabilities().FIREFOX
+cap["marionette"] = False
+driver = webdriver.Firefox(capabilities=cap, executable_path='C:\Program Files\geckodriver.exe')
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = driver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #Edith has heard about a cool new online to-do app.
+        #She goes to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        #She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+        #She is invited to enter a to-do iten straight away
+        #[...rest of comments as before]
+
+if __name__ == '__main__':
+    unittest.main()
